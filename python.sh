aba134e0778d93e53184ad04f92eea7dc782dda1
alexphilip5 <124353445+alexphilip5@users.noreply.github.com>
2023-02-06 10:22:42 +0530

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,43 @@
+! /bin/bash
+#Installing all the dependencies
+set -x
+yum -y groupinstall "Development Tools"
+sleep 1 m
+yum -y install gcc openssl-devel bzip2-devel libffi-devel
+sleep 1 m
+cd /root
+#Installing python from source code
+wget https://www.python.org/ftp/python/3.8.4/Python-3.8.4.tgz
+sleep 1 m
+tar -xvf Python-3.8.4.t! /bin/bash
+#Installing all the dependencies
+set -x
+yum -y groupinstall "Development Tools"
+sleep 1 m
+yum -y install gcc openssl-devel bzip2-devel libffi-devel
+sleep 1 m
+cd /root
+#Installing python from source code
+wget https://www.python.org/ftp/python/3.8.4/Python-3.8.4.tgz
+sleep 1 m
+tar -xvf Python-3.8.4.tgz
+sleep 1 m
+./configure --enable-optimizations --with-ensurepip=install
+sleep 10 m
+make -j 8
+sleep 10 m
+make altinstall
+sleep 10 m
+# Installing git
+yum install git -y
+sleep 1 mgz
+sleep 1 m
+./configure --enable-optimizations --with-ensurepip=install
+sleep 10 m
+make -j 8
+sleep 10 m
+make altinstall
+sleep 10 m
+# Installing git
+yum install git -y
+sleep 1 m
